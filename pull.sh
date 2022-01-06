a51825449ead27b5a77b45eb3520a1aac4b8c467
James Eagle <jimeagle@gmail.com>
2022-01-06 21:58:20 +0000

    Remove scripts

--- pull.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git fetch origin
-git reset --hard origin/master
--- wakealarm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sh -c "echo 0 > /sys/class/rtc/rtc0/wakealarm"
-sh -c "echo `date '+%s' -d '+ 12 hours'` > /sys/class/rtc/rtc0/wakealarm"
-
-/sbin/shutdown -h now
