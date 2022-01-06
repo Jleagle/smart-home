681f544e607bf24df982be28b90dc1484bc2404c
Jleagle <jimeagle@gmail.com>
2022-01-06 13:32:35 +0000

    Update move

--- move.sh
+++ move.sh
@@ -1,4 +1,5 @@
-rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh ${MEDIA_DATA}/ james@192.168.0.7:/home/james/Incoming/Content
+rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh /mnt/96848BE4848BC4E9/Television james@192.168.0.7:/home/james/Incoming/Content/Television
+rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh /mnt/96848BE4848BC4E9/Video james@192.168.0.7:/home/james/Incoming/Content/Movies
 rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh ${DOCKER_DATA}/ james@192.168.0.7:/home/james/Incoming/Docker
 
 #  --stats \
