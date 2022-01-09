c9159fb09d7e63432e8d80e87ae1c9ec5fdec867
Jleagle <jimeagle@gmail.com>
2022-01-09 15:54:32 +0000

    Stats

--- move.sh
+++ move.sh
@@ -1,6 +1,6 @@
-rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh /mnt/96848BE4848BC4E9/Television james@192.168.0.7:/home/james/Incoming/Content/Television
-rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh /mnt/96848BE4848BC4E9/Video james@192.168.0.7:/home/james/Incoming/Content/Video
-rsync --human-readable --recursive --verbose --relative --links --times --delete --force -e ssh /home/james/docker-data james@192.168.0.7:/home/james/Incoming/Docker
+rsync --human-readable --recursive --stats --verbose --relative --links --times --delete --force -e ssh /mnt/96848BE4848BC4E9/Television james@192.168.0.7:/home/james/Incoming/Content/Television
+rsync --human-readable --recursive --stats --verbose --relative --links --times --delete --force -e ssh /mnt/96848BE4848BC4E9/Video james@192.168.0.7:/home/james/Incoming/Content/Video
+rsync --human-readable --recursive --stats --verbose --relative --links --times --delete --force -e ssh /home/james/docker-data james@192.168.0.7:/home/james/Incoming/Docker
 
 #  --stats \
 #  --progress \
