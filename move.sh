1e3781b80ae87cc1e286a8825767f25d76eeb3a3
James Eagle <jimeagle@gmail.com>
2022-01-11 20:54:18 +0000

    Remove move

--- move.sh
+++
@@ -1,7 +0,0 @@
-rsync --human-readable --recursive --stats --verbose --links --times -e ssh /mnt/96848BE4848BC4E9/Television james@192.168.0.7:/home/james/Incoming/Content/Television
-rsync --human-readable --recursive --stats --verbose --links --times -e ssh /mnt/96848BE4848BC4E9/Video/ james@192.168.0.7:/home/james/Media/Movies/
-rsync --human-readable --recursive --stats --verbose --links --times -e ssh /home/james/docker-data james@192.168.0.7:/home/james/Incoming/Docker
-
-#  --stats \
-#  --progress \
-#  --compress \
