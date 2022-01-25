28d12bf2b740288129d93d408399eb2a7c3f41d0
James Eagle <jimeagle@gmail.com>
2022-01-25 19:57:42 +0000

    Update init

--- influx/init.sh
+++ influx/init.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 docker exec influx influx setup \
-      --username $INFLUX_USERNAME \
-      --password $INFLUX_PASSWORD
+  --username $INFLUX_USERNAME \
+  --password $INFLUX_PASSWORD
+
+docker exec -it influx influx v1 dbrp create \
+  --db Telegraf \
+  --rp Telegraf \
+  --bucket-id $BUCKET_ID \
+  --token $ADMIN_TOKEN \
+  --org Seven \
+  --default
