fbc14d446283ce05bc8bccd40a13169889cf6f40
Jleagle <jimeagle@gmail.com>
2022-02-04 13:06:43 +0000

    Backup

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run --rm -v ${HOME}:/data \
+  -e RESTIC_REPOSITORY=s3:s3.amazonaws.com/bucket_name \
+  -e RESTIC_PASSWORD=my-secure-password \
+  -e AWS_ACCESS_KEY_ID=my-aws-access-key \
+  -e AWS_SECRET_ACCESS_KEY=my-aws-secret-key \
+  instrumentisto/restic backup /data
