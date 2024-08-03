7798d0f904fef8fbeb72445b20b07c982ca203e6
Jleagle <jimeagle@gmail.com>
2024-08-03 13:02:34 +0100

    Only build local images

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ bash ./pull.sh
 . ~/.bashrc
 
 docker compose pull
-docker compose up --detach --build --remove-orphans
+docker compose up --detach --remove-orphans
+docker compose up --detach --build library cameras
 
 docker system prune --all --force
