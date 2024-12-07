a00c625c6287e2915eb9a6fc3ee07b0bf3d68da2
Jleagle <jimeagle@gmail.com>
2024-12-07 20:39:01 +0000

    Backup

--- backup.sh
+++ backup.sh
@@ -2,9 +2,26 @@
 
 # https://restic.readthedocs.io/en/latest/index.html
 
-docker run --rm -v ${HOME}:/data \
-  -e RESTIC_REPOSITORY=s3:s3.amazonaws.com/bucket_name \
-  -e RESTIC_PASSWORD=my-secure-password \
-  -e AWS_ACCESS_KEY_ID=my-aws-access-key \
-  -e AWS_SECRET_ACCESS_KEY=my-aws-secret-key \
-  instrumentisto/restic backup /data
+#docker run --rm -v ${HOME}:/data \
+#  -e RESTIC_REPOSITORY=s3:s3.amazonaws.com/bucket_name \
+#  -e RESTIC_PASSWORD=my-secure-password \
+#  -e AWS_ACCESS_KEY_ID=my-aws-access-key \
+#  -e AWS_SECRET_ACCESS_KEY=my-aws-secret-key \
+#  instrumentisto/restic backup /data
+
+rsync \
+	--dry-run \
+	--archive \
+	--compress \
+	--progress \
+	--partial \
+	--human-readable \
+	--rsh 'ssh -oPort=2222' \
+	--exclude="Media" \
+	--exclude="nas-backup/" \
+	--exclude="nas-backup2/" \
+	--exclude="Tautulli/cache/" \
+	--exclude="Repo/" \
+	--exclude="Plex/Config/Library/Application\ Support/Plex\ Media\ Server/Media/" \
+	--exclude="Plex/Config/Library/Application\ Support/Plex\ Media\ Server/Cache/" \
+	seven:/home/james ~/Downloads/Seven/
