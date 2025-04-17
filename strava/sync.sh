aac2275670ed380f530566be98dcfa218c3af85f
Jleagle <jimeagle@gmail.com>
2025-04-17 12:38:41 +0100

    Reorder strava script

--- strava/sync.sh
+++ strava/sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker compose exec strava bin/console app:strava:build-files
 docker compose exec strava bin/console app:strava:import-data
+docker compose exec strava bin/console app:strava:build-files
