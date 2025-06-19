47a34349df7179d554f256e02ea61f999d6a78a6
Jleagle <jimeagle@gmail.com>
2025-06-19 20:40:43 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -2,68 +2,68 @@
 
 mkdir -p ~/AdGuard/Conf
 mkdir -p ~/AdGuard/Work
-#mkdir -p ~/AssistantRelay/Responses
-#mkdir -p ~/Authentik/Certs
-#mkdir -p ~/Authentik/Media
-#mkdir -p ~/Authentik/Templates
 mkdir -p ~/Bazarr
 mkdir -p ~/Beszel
 mkdir -p ~/ChangeDetection
-mkdir -p ~/CodeProjectAI
-mkdir -p ~/Deepstack
 mkdir -p ~/DDNS
+mkdir -p ~/Deepstack
 mkdir -p ~/DoubleTake
-mkdir -p ~/Elastic
-#mkdir -p ~/Go2rtc/Config
-mkdir -p ~/FlexTV
 mkdir -p ~/Frigate
-mkdir -p ~/Grafana/Data
 mkdir -p ~/Hass
-#mkdir -p ~/Homepage
 mkdir -p ~/Huntarr
-mkdir -p ~/Influx
-mkdir -p ~/Kibana
-mkdir -p ~/Mass
-#mkdir -p ~/Matrix
-#mkdir -p ~/Mealie
 mkdir -p ~/Media/Frigate
-mkdir -p ~/Media/Slskd
 mkdir -p ~/Media/Movies
+mkdir -p ~/Media/Slskd
 mkdir -p ~/Media/Television
 mkdir -p ~/Media/YouTube
-mkdir -p ~/Minecraft
-#mkdir -p ~/Mosquitto/Data
-#mkdir -p ~/Mosquitto/Log
 mkdir -p ~/NodeRed
 mkdir -p ~/NZBGet/Config
 mkdir -p ~/NZBGet/Downloads
-mkdir -p ~/PiHole
 mkdir -p ~/Plex/Config
 mkdir -p ~/Plex/Transcoding
-mkdir -p ~/PlexMeta
-mkdir -p ~/Postgres/Data
 mkdir -p ~/Prowlarr
 mkdir -p ~/Radarr
 mkdir -p ~/Redis/Data
-#mkdir -p ~/SerpBear
 mkdir -p ~/Slskd
 mkdir -p ~/Sonarr
 mkdir -p ~/Strava
 mkdir -p ~/Syncthing/Config
 mkdir -p ~/Syncthing/Data
-mkdir -p ~/Tautulli
-#mkdir -p ~/Technitium
 mkdir -p ~/Traefik
 mkdir -p ~/UptimeKuma
+#mkdir -p ~/AssistantRelay/Responses
+#mkdir -p ~/Authentik/Certs
+#mkdir -p ~/Authentik/Media
+#mkdir -p ~/Authentik/Templates
+#mkdir -p ~/CodeProjectAI
+#mkdir -p ~/Elastic
+#mkdir -p ~/FlexTV
+#mkdir -p ~/Go2rtc/Config
+#mkdir -p ~/Grafana/Data
+#mkdir -p ~/Homepage
+#mkdir -p ~/Influx
+#mkdir -p ~/Kibana
+#mkdir -p ~/Mass
+#mkdir -p ~/Matrix
+#mkdir -p ~/Mealie
+#mkdir -p ~/Minecraft
+#mkdir -p ~/Mosquitto/Data
+#mkdir -p ~/Mosquitto/Log
+#mkdir -p ~/PiHole
+#mkdir -p ~/PlexMeta
+#mkdir -p ~/Postgres/Data
+#mkdir -p ~/SerpBear
+#mkdir -p ~/Tautulli
+#mkdir -p ~/Technitium
 #mkdir -p ~/Varken
-mkdir -p ~/Zinc
+#mkdir -p ~/Zinc
 
 touch ~/Auth/config.json
-touch ~/AssistantRelay/config.json
-touch ~/DDNS/records.yaml
 touch ~/Frigate/config.yml
 touch ~/Hass/assistant_service_account.json
-#touch ~/Mosquitto/Log/mosquitto.log
 touch ~/Traefik/acme.json
+#touch ~/AssistantRelay/config.json
+#touch ~/DDNS/records.yaml
+#touch ~/Mosquitto/Log/mosquitto.log
 
 chmod 600 ~/Traefik/acme.json
