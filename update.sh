bb75bb9d85236c39d5de758e84596b61d21f6a62
James Eagle <jimeagle@gmail.com>
2026-01-26 13:28:08 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-bash ./pull.sh
+# Pull latest changes from git
+if [ -f "./pull.sh" ]; then
+  bash ./pull.sh
+fi
 
-. ~/.bashrc
+# Load bashrc if needed (optional, might not be necessary in non-interactive shell)
+[ -f ~/.bashrc ] && . ~/.bashrc
 
+echo "Pulling latest images..."
 docker compose pull
-docker compose up --detach --remove-orphans
-docker compose restart traefik
 
-docker system prune --all --force &
+echo "Starting services..."
+docker compose up --detach --remove-orphans
+
+echo "Cleaning up old images..."
+docker system prune --force
