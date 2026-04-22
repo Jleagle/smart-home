4956377e08ade3d0ec978a3e896398ef6e979134
James Eagle <jimeagle@gmail.com>
2026-04-22 21:55:43 +0100

    Update mounts

--- setup.sh
+++ setup.sh
@@ -16,7 +16,6 @@ mkdir -p ~/Media/Television
 mkdir -p ~/Media/YouTube
 mkdir -p ~/NodeRed
 mkdir -p ~/NzbDav
-mkdir -p ~/NzbDav/Downloads
 mkdir -p ~/NZBGet/Config
 mkdir -p ~/NZBGet/Downloads
 mkdir -p ~/Plex/Config
